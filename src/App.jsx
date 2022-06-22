e1c4d74c8ae554f68832dc80ba2e24f008826a39
Edward Prado <edward.prado@weareadaptive.com>
2022-06-22 15:46:21 -0400

    feat: add base markup and styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,19 @@ import "./theme/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="stopwatch-container">
+      <header className="stopwatch-header">
+        <h2>00:00.00</h2>
+      </header>
+      <div>
+        <section className="controls">
+          <button id="reset-lap">Reset</button>
+          <button id="stop-start" className="start">
+            Start
+          </button>
+        </section>
+        <section className="laps-container" aria-label="Lap history"></section>
+      </div>
     </div>
   );
 }
