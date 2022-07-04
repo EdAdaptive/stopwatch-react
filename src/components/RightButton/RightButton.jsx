0b5f02596e891ffd9903058d3f78e90d34a4ec14
Edward Prado <edward.prado@weareadaptive.com>
2022-07-04 14:09:10 -0400

    feat: add right button

---
+++ src/components/RightButton/RightButton.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./rightButton.css";
+
+export default function RightButton(props) {
+  function startStopwatch() {
+    props.setIsTiming(true);
+    if (props.startTime === 0) {
+      props.setStartTime(Date.now());
+    }
+  }
+
+  function stopStopwatch() {
+    props.setIsTiming(false);
+  }
+
+  return (
+    <button
+      className={props.isTiming ? "stop" : "start"}
+      onClick={props.isTiming ? stopStopwatch : startStopwatch}
+    >
+      {props.isTiming ? "Stop" : "Start"}
+    </button>
+  );
+}
